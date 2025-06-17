ce310d888e5a5b643aa65093ba8be7948180d236
dianaseong <dianaseong31@gmail.com>
2025-06-17 17:03:16 +0900

    update url

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwtAxios from './jwtAxios';
 
-const MEMBER_SERVICE_URL = "https://www.inkcloud.click/api/v1/members"
+// const MEMBER_SERVICE_URL = "https://www.inkcloud.click/api/v1/members"
 
 // const MEMBER_SERVICE_URL = "http://api-gateway-service.inkcloud.svc.cluster.local/api/v1/members"
 // const MEMBER_SERVICE_URL = process.env.REACT_APP_PREFIX_URL +"/members";
