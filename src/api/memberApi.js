3ddc79839385fb7e8f09b6ccfc1120299e239ea3
dianaseong <dianaseong31@gmail.com>
2025-06-17 16:14:15 +0900

    update env

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import jwtAxios from './jwtAxios';
 
-const MEMBER_SERVICE_URL = "http://api-gateway-service.inkcloud.svc.cluster.local/api/v1/members"
-// const MEMBER_SERVICE_URL = REACT_APP_PREFIX_URL + "/members";
+// const MEMBER_SERVICE_URL = "http://api-gateway-service.inkcloud.svc.cluster.local/api/v1/members"
+const MEMBER_SERVICE_URL = `${REACT_APP_PREFIX_URL}/members`;
 // const MEMBER_SERVICE_URL = "http://localhost:25000/api/v1/members";
 
 //회원가입시 인증번호 발송
