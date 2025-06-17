7da73166cf5f533a3f6f16620d1389d608faeb5f
dianaseong <dianaseong31@gmail.com>
2025-06-17 16:44:20 +0900

    update url

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import jwtAxios from './jwtAxios';
 
 const MEMBER_SERVICE_URL = "https://www.inkcloud.click/api/v1/members"
+
 // const MEMBER_SERVICE_URL = "http://api-gateway-service.inkcloud.svc.cluster.local/api/v1/members"
 // const MEMBER_SERVICE_URL = process.env.REACT_APP_PREFIX_URL +"/members";
 // const MEMBER_SERVICE_URL = "http://localhost:25000/api/v1/members";
@@ -9,7 +10,7 @@ const MEMBER_SERVICE_URL = "https://www.inkcloud.click/api/v1/members"
 //회원가입시 인증번호 발송
 export const SendVerificationEmail = async(email) => {
   const response = await axios.post(
-      `${MEMBER_SERVICE_URL}/signup/email/send`, { email },  
+      `/api/v1/members/signup/email/send`, { email },  
     {
     timeout: 10000, // 5초
     }
