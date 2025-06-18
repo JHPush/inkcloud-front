0f698f770d610022b7030a1d804bfdf8a1a731a6
PoorHotsix <majch98@gmail.com>
2025-06-18 15:29:58 +0900

    Test List Bucket

--- src/components/admin/ProductForm.jsx
+++ src/components/admin/ProductForm.jsx
@@ -58,7 +58,7 @@ const ProductForm = ({ product, onClose }) => {
     await publicApi.put(presignedUrl, imageFile, {
       headers: {
         "Content-Type": imageFile.type,
-        'x-amz-acl': 'bucket-owner-full-control',
+        //'x-amz-acl': 'bucket-owner-full-control',
       },
     });
 
