f8d8d6de0639904b44f28bd07a7a81e7baad68e3
PoorHotsix <majch98@gmail.com>
2025-06-23 01:17:05 +0900

    cart icon

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 // src/store/index.js
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './loginSlice';
+import cartReducer from './cartSlice'
 
 const store = configureStore({
   reducer: {
